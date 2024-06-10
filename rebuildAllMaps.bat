ce7e106b9939dfb82eda396a809f314fe7af3971
Roman Shapiro <eleroman@mail.ru>
2024-06-11 01:29:33 +0700

    Areas fixes

---
+++ rebuildAllMaps.bat
@@ -0,0 +1,17 @@
+del /Q "data\circle31\maps\png\*.png"
+MUDMapBuilder.BatchConverter.exe "data\circle31\maps\json" "data\circle31\maps\png" >circle31.txt
+
+del /Q "data\circle35\maps\png\*.png"
+MUDMapBuilder.BatchConverter.exe "data\circle35\maps\json" "data\circle35\maps\png" >circle35.txt
+
+del /Q "data\tbaMUD\maps\png\*.png"
+MUDMapBuilder.BatchConverter.exe "data\tbaMUD\maps\json" "data\tbaMUD\maps\png" >tbaMUD.txt
+
+del /Q "data\Envy22\maps\png\*.png"
+MUDMapBuilder.BatchConverter.exe "data\Envy22\maps\json" "data\Envy22\maps\png" >Envy22.txt
+
+del /Q "data\ROM24b6\maps\png\*.png"
+MUDMapBuilder.BatchConverter.exe "data\ROM24b6\maps\json" "data\ROM24b6\maps\png" >ROM24b6.txt
+
+del /Q "data\CSL\maps\png\*.png"
+MUDMapBuilder.BatchConverter.exe "data\CSL\maps\json" "data\CSL\maps\png" >CSL.txt
