6312f94a287cfa90c614bb02b7a82d20df737ff0
Roman Shapiro <eleroman@mail.ru>
2024-06-11 13:28:11 +0700

    Used new naming of batch converter

--- rebuildAllMaps.bat
+++ rebuildAllMaps.bat
@@ -1,17 +1,17 @@
 del /Q "data\circle31\maps\png\*.png"
-MUDMapBuilder.BatchConverter.exe "data\circle31\maps\json" "data\circle31\maps\png" >circle31.txt
+mmb-bc.exe "data\circle31\maps\json" "data\circle31\maps\png" >circle31.txt
 
 del /Q "data\circle35\maps\png\*.png"
-MUDMapBuilder.BatchConverter.exe "data\circle35\maps\json" "data\circle35\maps\png" >circle35.txt
+mmb-bc.exe "data\circle35\maps\json" "data\circle35\maps\png" >circle35.txt
 
 del /Q "data\tbaMUD\maps\png\*.png"
-MUDMapBuilder.BatchConverter.exe "data\tbaMUD\maps\json" "data\tbaMUD\maps\png" >tbaMUD.txt
+mmb-bc.exe "data\tbaMUD\maps\json" "data\tbaMUD\maps\png" >tbaMUD.txt
 
 del /Q "data\Envy22\maps\png\*.png"
-MUDMapBuilder.BatchConverter.exe "data\Envy22\maps\json" "data\Envy22\maps\png" >Envy22.txt
+mmb-bc.exe "data\Envy22\maps\json" "data\Envy22\maps\png" >Envy22.txt
 
 del /Q "data\ROM24b6\maps\png\*.png"
-MUDMapBuilder.BatchConverter.exe "data\ROM24b6\maps\json" "data\ROM24b6\maps\png" >ROM24b6.txt
+mmb-bc.exe "data\ROM24b6\maps\json" "data\ROM24b6\maps\png" >ROM24b6.txt
 
 del /Q "data\CSL\maps\png\*.png"
-MUDMapBuilder.BatchConverter.exe "data\CSL\maps\json" "data\CSL\maps\png" >CSL.txt
+mmb-bc.exe "data\CSL\maps\json" "data\CSL\maps\png" >CSL.txt
