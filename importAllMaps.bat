f7f31745073cc537cdad53c8114173a2a1cfe859
Roman Shapiro <eleroman@mail.ru>
2025-02-03 08:45:43 +0700

    Maps with doors/keys

--- importAllMaps.bat
+++ importAllMaps.bat
@@ -1,17 +1,6 @@
-del /Q "data\circle31\maps\json\*.json"
 MUDMapBuilder.Import.Console.exe circle31 "D:\Projects\chaos\circle-3.1\lib\world" "data\circle31\maps\json" >circle31.import.txt
-
-del /Q "data\circle35\maps\json\*.json"
 MUDMapBuilder.Import.Console.exe circle35 "D:\Projects\chaos\circle-3.5\lib\world" "data\circle35\maps\json" >circle35.import.txt
-
-del /Q "data\tbaMUD\maps\json\*.json"
 MUDMapBuilder.Import.Console.exe tbaMUD "D:\Projects\chaos\tbamud\lib\world" "data\tbaMUD\maps\json" >tbaMUD.import.txt
-
-del /Q "data\Envy22\maps\json\*.json"
 MUDMapBuilder.Import.Console.exe Envy22 "D:\Projects\chaos\envy22\area" "data\Envy22\maps\json" >Envy22.import.txt
-
-del /Q "data\ROM24b6\maps\json\*.json"
 MUDMapBuilder.Import.Console.exe ROM24b6 "D:\Projects\chaos\ROM\area" "data\ROM24b6\maps\json" >ROM24b6.import.txt
-
-del /Q "data\CSL\maps\json\*.json"
 MUDMapBuilder.Import.Console.exe CSL "D:\Projects\CrimsonStainedLands\master\CrimsonStainedLands\data\areas" "data\CSL\maps\json" >CSL.import.txt
--- rebuildAllMaps.bat
+++ rebuildAllMaps.bat
@@ -1,17 +1,17 @@
 del /Q "data\circle31\maps\png\*.png"
-mmb-bc.exe "data\circle31\maps\json" "data\circle31\maps\png" >circle31.txt
+mmb-bc.exe "data\circle31\maps\json" "data\circle31\maps\png" >circle31.mmb.txt
 
 del /Q "data\circle35\maps\png\*.png"
-mmb-bc.exe "data\circle35\maps\json" "data\circle35\maps\png" >circle35.txt
+mmb-bc.exe "data\circle35\maps\json" "data\circle35\maps\png" >circle35.mmb.txt
 
 del /Q "data\tbaMUD\maps\png\*.png"
-mmb-bc.exe "data\tbaMUD\maps\json" "data\tbaMUD\maps\png" >tbaMUD.txt
+mmb-bc.exe "data\tbaMUD\maps\json" "data\tbaMUD\maps\png" >tbaMUD.mmb.txt
 
 del /Q "data\Envy22\maps\png\*.png"
-mmb-bc.exe "data\Envy22\maps\json" "data\Envy22\maps\png" >Envy22.txt
+mmb-bc.exe "data\Envy22\maps\json" "data\Envy22\maps\png" >Envy22.mmb.txt
 
 del /Q "data\ROM24b6\maps\png\*.png"
-mmb-bc.exe "data\ROM24b6\maps\json" "data\ROM24b6\maps\png" >ROM24b6.txt
+mmb-bc.exe "data\ROM24b6\maps\json" "data\ROM24b6\maps\png" >ROM24b6.mmb.txt
 
 del /Q "data\CSL\maps\png\*.png"
-mmb-bc.exe "data\CSL\maps\json" "data\CSL\maps\png" >CSL.txt
+mmb-bc.exe "data\CSL\maps\json" "data\CSL\maps\png" >CSL.mmb.txt
